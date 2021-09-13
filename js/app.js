98b6b99389135efe728aa0f2c09f6416d938ca42
Tasnim <tasnim.mostari@gmail.com>
2021-09-13 16:53:18 +0600

    finished

--- js/app.js
+++ js/app.js
@@ -9,17 +9,19 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    // console.log(product.price);
+
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `<div class="single-product bg-success p-2 text-dark bg-opacity-10">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <p>Rating: ${product.rating.rate}</p>
+      <p>Count: ${product.rating.count}</p>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
